ea1cad29ca47e1ed26e47b5abbbf847ac7a8b7a3
Sergey Kolokolov <serg.a.bells@gmail.com>
2022-10-29 13:02:55 -0700

    refactors StepperPage object

--- cypress/e2e/ngx-admin/page-objects/pages/StepperPage.ts
+++ cypress/e2e/ngx-admin/page-objects/pages/StepperPage.ts
@@ -2,14 +2,18 @@ import BasePage from "./BasePage";
 
 export default class StepperPage extends BasePage {
     public static readonly URL = "http://localhost:4200/pages/layout/stepper"
-    public static readonly NB_CARD = "nb-card nb-card-body"
-    public static readonly STEP = ".step"
-    public static readonly STEP_INDEX = ".label-index"
-    public static readonly STEP_LABEL = ".label"
     public static readonly STEP_LABEL_TEXT1 = "First step"
     public static readonly STEP_LABEL_TEXT2 = "Second step"
     public static readonly STEP_LABEL_TEXT3 = "Third step"
     public static readonly STEP_LABEL_TEXT4 = "Fourth step"
+
+    public static elements = {
+        stepperCard: (cardNo: number) => { return cy.get("nb-card nb-card-body").eq(cardNo - 1 ) },
+        stepperSteps: (cardNo: number) => { return this.elements.stepperCard(cardNo).find(".step") },
+        stepperStep: (cardNo: number, stepNo: number) => { return this.elements.stepperSteps(cardNo).eq(stepNo - 1 ) },
+        stepIndex: () => { return cy.get(".label-index") },
+        stepLabel: () => { return cy.get(".label") },
+    }
 
     public static visitPage(): void {
         cy.visit(this.URL);
@@ -20,15 +24,13 @@ export default class StepperPage extends BasePage {
     }
 
     public static validateStepperCardVisible(cardNo: number): void {
-        cy.get(this.NB_CARD).eq(cardNo - 1 ).should('be.visible');
+        this.elements.stepperCard(cardNo).should('be.visible');
     }
 
     public static validateStepLabelsVisible(cardNo: number, stepsCount: number): void {
-        cy.get(this.NB_CARD).eq(cardNo - 1).then(card => {
-            cy.wrap(card).find(this.STEP_LABEL).each((label, index) => {
-                cy.wrap(label).contains(this[`STEP_LABEL_TEXT${index + 1}`]);
+        this.elements.stepperSteps(cardNo).each((step, index) => {
+                cy.wrap(step).contains(this[`STEP_LABEL_TEXT${index + 1}`]);
             });
-        });
     }
 
 }
