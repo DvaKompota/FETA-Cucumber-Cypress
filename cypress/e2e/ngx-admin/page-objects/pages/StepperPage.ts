bc662ad3349d1ee478167648a96af5534550d77b
Sergey Kolokolov <serg.a.bells@gmail.com>
2022-10-29 11:06:22 -0700

    updates Layout Stepper happy elements test

--- cypress/e2e/ngx-admin/page-objects/pages/StepperPage.ts
+++ cypress/e2e/ngx-admin/page-objects/pages/StepperPage.ts
@@ -2,13 +2,33 @@ import BasePage from "./BasePage";
 
 export default class StepperPage extends BasePage {
     public static readonly URL = "http://localhost:4200/pages/layout/stepper"
+    public static readonly NB_CARD = "nb-card nb-card-body"
+    public static readonly STEP = ".step"
+    public static readonly STEP_INDEX = ".label-index"
+    public static readonly STEP_LABEL = ".label"
+    public static readonly STEP_LABEL_TEXT1 = "First step"
+    public static readonly STEP_LABEL_TEXT2 = "Second step"
+    public static readonly STEP_LABEL_TEXT3 = "Third step"
+    public static readonly STEP_LABEL_TEXT4 = "Fourth step"
 
     public static visitPage(): void {
-        cy.visit(this.URL)
+        cy.visit(this.URL);
     }
 
     public static validateURL(): void {
-        cy.url().should("contain", this.URL)
+        cy.url().should("contain", this.URL);
+    }
+
+    public static validateStepperCardVisible(cardNo: number): void {
+        cy.get(this.NB_CARD).eq(cardNo - 1 ).should('be.visible');
+    }
+
+    public static validateStepLabelsVisible(cardNo: number, stepsCount: number): void {
+        cy.get(this.NB_CARD).eq(cardNo - 1).then(card => {
+            cy.wrap(card).find(this.STEP_LABEL).each((label, index) => {
+                cy.wrap(label).contains(this[`STEP_LABEL_TEXT${index + 1}`]);
+            });
+        });
     }
 
 }
--- cypress/e2e/ngx-admin/tests/layout-stepper/layoutStepper.ts
+++ cypress/e2e/ngx-admin/tests/layout-stepper/layoutStepper.ts
@@ -9,3 +9,13 @@ When(/^user navigates to the Layout Stepper Page$/, () => {
 Then(/^Layout Stepper Page url is correct$/, () => {
     StepperPage.validateURL()
 });
+
+Then(/^Layout Stepper Page cards are visible$/, () => {
+    StepperPage.validateStepperCardVisible(1)
+    StepperPage.validateStepperCardVisible(2)
+    StepperPage.validateStepperCardVisible(3)
+});
+
+Then(/^Layout Stepper Card "(.*)" has "(.*)" step labels$/, (cardNo: string, stepsCount: string) => {
+    StepperPage.validateStepLabelsVisible(Number(cardNo), Number(stepsCount))
+});
